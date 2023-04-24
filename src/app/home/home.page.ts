f74028d3fcf63faf579829c7ccfc725ca0a8f6b5
Douglas Silva <douglas.silva@projuris.com.br>
2023-04-24 18:08:48 -0300

    adicionado display no home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
+import { DisplayComponent } from '../shared/components/display-component/display.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, DisplayComponent],
 })
 export class HomePage {
+  entity: string = 'Saldo';
+  totalValue: string = 'R$ 2.000,00'
 
   constructor(private router: Router) {}
 
